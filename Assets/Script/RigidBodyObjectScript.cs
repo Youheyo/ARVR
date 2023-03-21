8cce8e52d7806e3149714378acb77deb079bd559
Youheyo <80012381+Youheyo@users.noreply.github.com>
2023-03-22 01:14:17 +0800

    Added "pooling" for fallen cubes
    When cubes fall a max distance, it respawns back to its original position.

--- Assets/Script/RigidBodyObjectScript.cs
+++ Assets/Script/RigidBodyObjectScript.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class RigidBodyObjectScript : MonoBehaviour
 {
 
+Vector3 OriginalPos;
+[SerializeField] float maxDistanceFromOriginalPos = 1000;
+
+public enum RespawnType{Original, Set};
+
+public RespawnType respawnType;
     Rigidbody rb;
 
     // Start is called before the first frame update
@@ -12,8 +18,23 @@ public class RigidBodyObjectScript : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.useGravity = false;
+
+        OriginalPos = transform.position;
     }
-
     
-
+    void Update(){
+        switch(respawnType){
+            case RespawnType.Original: 
+                if(Vector3.Distance(OriginalPos, this.transform.position) > maxDistanceFromOriginalPos){
+                    transform.position = OriginalPos;
+                    rb.velocity = Vector3.zero;
+                }
+            break;
+            case RespawnType.Set:
+            break;
+            default:
+                Debug.Log("Unkown Respawn Type");
+            break;
+        }
+    }
 }
